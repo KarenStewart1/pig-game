d7e00576fa91f9233e3b8606e4c749f94921ab78
Karen Stewart <karenstewart1117@gmail.com>
2022-01-19 12:56:24 +0100

    change "class" to "className" in start files

--- src/App.js
+++ src/App.js
@@ -3,38 +3,38 @@ function App() {
   return (
     <div className="App">
       <main>
-        <section class="player player--0 player--active">
-          <h2 class="name" id="name--0">
+        <section className="player player--0 player--active">
+          <h2 className="name" id="name--0">
             Player 1
           </h2>
-          <p class="score" id="score--0">
+          <p className="score" id="score--0">
             43
           </p>
-          <div class="current">
-            <p class="current-label">Current</p>
-            <p class="current-score" id="current--0">
+          <div className="current">
+            <p className="current-label">Current</p>
+            <p className="current-score" id="current--0">
               0
             </p>
           </div>
         </section>
-        <section class="player player--1">
-          <h2 class="name" id="name--1">
+        <section className="player player--1">
+          <h2 className="name" id="name--1">
             Player 2
           </h2>
-          <p class="score" id="score--1">
+          <p className="score" id="score--1">
             24
           </p>
-          <div class="current">
-            <p class="current-label">Current</p>
-            <p class="current-score" id="current--1">
+          <div className="current">
+            <p className="current-label">Current</p>
+            <p className="current-score" id="current--1">
               0
             </p>
           </div>
         </section>
-        <img src="dice-5.png" alt="Playing dice" class="dice" />
-        <button class="btn btn--new">🔄 New game</button>
         <button class="btn btn--roll">🎲 Roll dice</button>
-        <button class="btn btn--hold">📥 Hold</button>
+        <img src="dice-5.png" alt="Playing dice" className="dice" />
+        <button className="btn btn--new">🔄 New game</button>
+        <button className="btn btn--hold">📥 Hold</button>
       </main>
     </div>
   );
