f039214c828582636c0f5b483014c4d012470189
Karen Stewart <karenstewart1117@gmail.com>
2022-01-20 18:54:06 +0100

    add: winner status to winner of game, color change for player box and inability to continue rolling the dice and refactor code

--- src/App.js
+++ src/App.js
@@ -14,43 +14,65 @@ function App() {
   const [totalScore1, setTotalScore1] = useState(0);
   const [currentScore2, setCurrentScore2] = useState(0);
   const [totalScore2, setTotalScore2] = useState(0);
-  const [whoseTurn, setWhoseTurn] = useState("player1");
+  const [whoseTurn, setWhoseTurn] = useState({ player1: true, player2: false });
   let diceArray = [dice1, dice2, dice3, dice4, dice5, dice6];
 
   function switchPlayer() {
-    setWhoseTurn(whoseTurn === "player1" ? "player2" : "player1");
+    setWhoseTurn(
+      whoseTurn.player1
+        ? { player1: false, player2: true }
+        : { player1: true, player2: false }
+    );
   }
 
   function handleDiceRoll(randomNum) {
-    setRandomNumber(randomNum);
-    if (randomNum === 1) {
-      whoseTurn === "player1" ? setCurrentScore1(0) : setCurrentScore2(0);
-      switchPlayer();
-    } else if (whoseTurn === "player1") {
-      setCurrentScore1(currentScore1 + randomNum);
-    } else {
-      setCurrentScore2(currentScore2 + randomNum);
+    if (whoseTurn.player1 !== "winner" && whoseTurn.player2 !== "winner") {
+      setRandomNumber(randomNum);
+      if (randomNum === 1) {
+        whoseTurn.player1 ? setCurrentScore1(0) : setCurrentScore2(0);
+        switchPlayer();
+      } else if (whoseTurn.player1) {
+        setCurrentScore1(currentScore1 + randomNum);
+      } else {
+        setCurrentScore2(currentScore2 + randomNum);
+      }
+    }
+  }
+  function updateScores(...players) {
+    if (players.includes("player1")) {
+      setTotalScore1(totalScore1 + currentScore1);
+      setCurrentScore1(0);
+    }
+    if (players.includes("player2")) {
+      setTotalScore2(totalScore2 + currentScore2);
+      setCurrentScore2(0);
     }
   }
 
   function handleHold(event) {
     event.preventDefault();
-    if (whoseTurn === "player1") {
-      setTotalScore1(totalScore1 + currentScore1);
-      setCurrentScore1(0);
+    if (totalScore1 + currentScore1 > 9) {
+      setWhoseTurn({ player1: "winner", player2: false });
+      updateScores("player1");
+    } else if (totalScore2 + currentScore2 > 9) {
+      setWhoseTurn({ player1: false, player2: "winner" });
+      updateScores("player2");
     } else {
-      setTotalScore2(totalScore2 + currentScore2);
-      setCurrentScore2(0);
+      updateScores("player1", "player2");
+      switchPlayer();
     }
-    switchPlayer();
   }
-  console.log(whoseTurn);
+
   return (
     <div className="App">
       <main>
         <section
           className={
-            whoseTurn === "player1" ? "player player--active" : "player"
+            whoseTurn.player1 === "winner"
+              ? "player player--winner"
+              : whoseTurn.player1
+              ? "player player--active"
+              : "player"
           }
         >
           <h2 className="name" id="name--0">
@@ -68,7 +90,11 @@ function App() {
         </section>
         <section
           className={
-            whoseTurn === "player2" ? "player player--active" : "player"
+            whoseTurn.player2 === "winner"
+              ? "player player--winner"
+              : whoseTurn.player2
+              ? "player player--active"
+              : "player"
           }
         >
           <h2 className="name" id="name--1">
