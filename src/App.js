89f66a6d1a739bebfa376dd912ed537d288d6ddf
Karen Stewart <karenstewart1117@gmail.com>
2022-01-24 12:43:58 +0100

    refactor code setting scores back to 0 and handlingHold function

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ function importAll(r) {
 const images = importAll(require.context("./images", false, /\.(png)$/));
 
 function App() {
+  const startingScores = { player1: 0, player2: 0 };
   const [randomNumber, setRandomNumber] = useState(null);
-  const [currentScore, setCurrentScore] = useState({ player1: 0, player2: 0 });
-  const [totalScore, setTotalScore] = useState({ player1: 0, player2: 0 });
+  const [currentScore, setCurrentScore] = useState(startingScores);
+  const [totalScore, setTotalScore] = useState(startingScores);
   const [whoseTurn, setWhoseTurn] = useState({ player1: true, player2: false });
 
   function switchPlayer() {
@@ -29,24 +30,26 @@ function App() {
     if (whoseTurn.player1 !== "winner" && whoseTurn.player2 !== "winner") {
       setRandomNumber(randomNum);
       if (randomNum === 1) {
-        setCurrentScore({ player1: 0, player2: 0 });
+        setCurrentScore(startingScores);
         switchPlayer();
       } else {
-        setCurrentScore(
-          whoseTurn.player1
-            ? { player1: currentScore.player1 + randomNum, player2: 0 }
-            : { player1: 0, player2: currentScore.player2 + randomNum }
-        );
+        setCurrentScore({
+          player1: whoseTurn.player1 ? currentScore.player1 + randomNum : 0,
+          player2: whoseTurn.player2 ? currentScore.player2 + randomNum : 0,
+        });
       }
     }
   }
 
   function handleHold(event) {
     event.preventDefault();
-    if (totalScore.player1 + currentScore.player1 > 99) {
-      setWhoseTurn({ player1: "winner", player2: false });
-    } else if (totalScore.player2 + currentScore.player2 > 99) {
-      setWhoseTurn({ player1: false, player2: "winner" });
+    let player1HasWon = totalScore.player1 + currentScore.player1 > 99;
+    let player2HasWon = totalScore.player2 + currentScore.player2 > 99;
+    if (player1HasWon || player2HasWon) {
+      setWhoseTurn({
+        player1: player1HasWon ? "winner" : null,
+        player2: player2HasWon ? "winner" : null,
+      });
     } else {
       switchPlayer();
     }
@@ -54,13 +57,13 @@ function App() {
       player1: totalScore.player1 + currentScore.player1,
       player2: totalScore.player2 + currentScore.player2,
     });
-    setCurrentScore({ player1: 0, player2: 0 });
+    setCurrentScore(startingScores);
   }
 
   function startNewGame(event) {
     event.preventDefault();
-    setCurrentScore({ player1: 0, player2: 0 });
-    setTotalScore({ player1: 0, player2: 0 });
+    setCurrentScore(startingScores);
+    setTotalScore(startingScores);
     setRandomNumber(null);
     setWhoseTurn({ player1: true, player2: false });
   }
@@ -100,17 +103,12 @@ function App() {
             <p className="current-score">{currentScore.player2}</p>
           </div>
         </section>
-
         <button className="btn btn--new" onClick={startNewGame}>
           🔄 New game
         </button>
         <Rules />
         {randomNumber ? (
-          <img
-            src={images[`dice-${randomNumber}.png`]}
-            alt="Playing dice"
-            className="dice"
-          />
+          <img src={images[`dice-${randomNumber}.png`]} alt="playing dice" />
         ) : null}
         <RollDice onDiceRoll={handleDiceRoll} />
         <button className="btn btn--hold" onClick={handleHold}>
